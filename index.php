1c8def3ebb5da38c925facff3b0cc63303fcdc9e
Felicidadifts16 <42788747+Felicidadifts16@users.noreply.github.com>
2019-11-17 21:11:53 -0300

    2 pureba commit
    cambiamos el titulo de la pagina en el head

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TP Noticias</title>
+    <title>TP </title>
 </head>
 <body>
 <div class="container p-4 col-md-5">
